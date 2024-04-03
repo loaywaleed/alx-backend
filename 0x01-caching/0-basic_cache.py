7772954b9836233810dc149e9b82fa1aedfdee90
loaywaleed <loaywaleed9@gmail.com>
2024-04-03 21:29:01 +0200

    basic cache class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic caching module in python
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BaseCache(BaseCaching):
+    """Class representing basic caching in python"""
+
+    def put(self, key, item):
+        """Function that adds data to cache"""
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Function that gets data from cache"""
+        return self.cache_data.get(key)
