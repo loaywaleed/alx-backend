478efd82ee36fd8644aff37ed0c5b855c6470c93
loaywaleed <loaywaleed9@gmail.com>
2024-04-10 01:07:19 +0200

    timezone selector

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+Flask app module
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """obtaining user from url (simulating)"""
+    user_id = request.args.get("login_as")
+    if user_id is not None and int(user_id) in users:
+        return users[int(user_id)]
+    return None
+
+
+class Config(object):
+    """I18N config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.before_request
+def before_request():
+    """function to obtain current user"""
+    g.user = get_user()
+
+
+@babel.timezoneselector
+def get_timezone():
+    """gets current timeozone"""
+    try:
+        if request.args.get("timezone"):
+            return pytz.timezone(request.args.get("timezone")).zone
+        if g.user and g.user.get("timezone"):
+            return pytz.timezone(g.user["timezone"]).zone
+    except pytz.exceptions.UnknownTimeZoneError:
+        pass
+    return "UTC"
+
+
+@babel.localeselector
+def get_locale():
+    """ method that gets locale from url"""
+    if request.args.get("locale") in app.config["LANGUAGES"]:
+        return request.args.get("locale")
+    if g.user and g.user.get("locale") in app.config["LANGUAGES"]:
+        return g.user["locale"]
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def home():
+    """Route that returns 1-index.html"""
+    return render_template("6-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
