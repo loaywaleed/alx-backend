5d759dbc87e01a9ec08021a645dc15305df0eb98
loaywaleed <loaywaleed9@gmail.com>
2024-04-10 00:41:17 +0200

    simulating logins

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Flask app module
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """obtaining user from url (simulating)"""
+    user_id = request.args.get("login_as")
+    if user_id is not None and int(user_id) in users:
+        return users[int(user_id)]
+    return None
+
+
+class Config(object):
+    """I18N config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ method that gets locale from url"""
+    if request.args.get("locale") in app.config["LANGUAGES"]:
+        return request.args.get("locale")
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def home():
+    """Route that returns 1-index.html"""
+    return render_template("4-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
